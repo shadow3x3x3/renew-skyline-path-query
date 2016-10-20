4a7568d2357f1c544673976cb2ae1b20994a73c7
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-10-20 11:33:57 +0800

    using accessor insead of attrs functions

--- skyline_path/strcture/edge.py
+++ skyline_path/strcture/edge.py
@@ -8,9 +8,3 @@ class Edge:
         self.dst = dst
         self.distance = attrs[0] # Distane value on first position by default.
         self.attrs = attrs
-
-    def attrs(self, index = None):
-        if index is None:
-            return self.attrs
-        else:
-            return self.attrs[index]
