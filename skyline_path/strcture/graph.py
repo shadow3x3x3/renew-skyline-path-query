7aa3b4ae161c1bc2734d48b8d408f0d1ae2af8ff
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-10-20 13:00:07 +0800

    add init_from_edges and __init_nodes_from_edges

--- skyline_path/strcture/graph.py
+++ skyline_path/strcture/graph.py
@@ -1,4 +1,19 @@
 class Graph:
+    """
+    Record Basic Graph Data.
+    """
     def __init__(self):
-        self.nodes = []
-        self.edges = []
+        self.nodes = ()
+
+    def init_from_edges(self, edges):
+        """
+        Initialize form edges.
+        When edges read, add nodes automatic from edges.
+        """
+        self.edges = tuple(edges)
+        self.__init_nodes_from_edges()
+
+    def __init_nodes_from_edges(self):
+        for edge in self.edges:
+            if edge.src not in self.nodes: self.nodes += (edge.src, )
+            if edge.dst not in self.nodes: self.nodes += (edge.dst, )
