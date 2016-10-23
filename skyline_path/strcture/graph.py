ca1a6326fb960ca580cc071b075d0ae6fd86e87e
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-10-23 17:37:31 +0800

    add neighbors

--- skyline_path/strcture/graph.py
+++ skyline_path/strcture/graph.py
@@ -4,6 +4,7 @@ class Graph:
     """
     def __init__(self):
         self.nodes = ()
+        self.neighbors = {}
 
     def init_from_edges(self, edges):
         """
@@ -12,6 +13,7 @@ class Graph:
         """
         self.edges = tuple(edges)
         self.__init_nodes_from_edges()
+        self.__init_neighbors()
 
     def __init_nodes_from_edges(self):
         for edge in self.edges:
@@ -19,3 +21,16 @@ class Graph:
                 self.nodes += (edge.src, )
             if edge.dst not in self.nodes: 
                 self.nodes += (edge.dst, )
+    
+    def __init_neighbors(self):
+        for node in self.nodes:
+            self.neighbors[node] = self.__find_neighbors_by(node)
+
+    def __find_neighbors_by(self, node):
+        result = ()
+        for edge in self.edges:
+            if edge.src == node:
+                result += (edge.dst, )
+            if edge.dst == node:
+                result += (edge.src, )
+        return result
